b5ef6b3f3503921dfbfa25f06d3344541f44d16b
hell-hall <90412878+hell-hall@users.noreply.github.com>
2024-12-23 00:33:28 -0600

    Add files via upload

---
+++ DigitalClockVB/Module1.vb
@@ -0,0 +1,21 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.Clear()
+
+        While True
+
+            Dim currentTime As String = DateTime.Now.ToString("hh:mm:ss tt")
+
+            Console.SetCursorPosition(0, 0)
+            Console.WriteLine("Digital Clock")
+            Console.WriteLine("=======================")
+            Console.WriteLine($"Time: {currentTime}")
+
+            'Pause time
+            Threading.Thread.Sleep(1000) ' 1 Second
+        End While
+
+    End Sub
+
+End Module
